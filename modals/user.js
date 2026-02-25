d6a912e1318041c35f5d4daccc9a0f849401fbfc
Mazahir Mehdi <syedmazahir099@gmail.com>
2026-02-26 01:32:03 +0500

    initial project setup + boilerplate code

---
+++ modals/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/halloffame");
+
+let userScheme = mongoose.Schema({
+    name: String,
+    email: String,
+    imgurl: String
+});
+
+module.export = mongoose.model("user",userScheme);
